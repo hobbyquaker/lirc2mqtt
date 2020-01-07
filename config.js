9889031f4df9e0d4f56401580c0dce1a55ea9bce
bvmensvoort <info@bvmensvoort.nl>
2020-01-07 21:12:09 +0100

    Add possibility to connect with username and password

--- config.js
+++ config.js
@@ -3,12 +3,16 @@ const config = require('yargs')
     .describe('v', 'possible values: "error", "warn", "info", "debug"')
     .describe('n', 'instance name. used as mqtt client id and as prefix for connected topic')
     .describe('u', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
+    .describe('c', 'mqtt broker username credential')
+    .describe('w', 'mqtt broker password credential')
     .describe('l', 'lircd host')
     .describe('p', 'lircd port')
     .describe('h', 'show help')
     .alias({
         h: 'help',
         n: 'name',
+        c: 'username',
+        w: 'password',
         u: 'url',
         v: 'verbosity',
         l: 'lirc-host',
